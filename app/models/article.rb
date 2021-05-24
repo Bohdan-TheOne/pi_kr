02efc8b452d9d094bb64f5ad78d68ad22096c51a
Bohdan Ch <BohdanCh.the.One@gmail.com>
2021-05-24 21:40:28 +0300

    Change article validation and error output

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ApplicationRecord
     include Visible
     has_many :comments, dependent: :destroy
 
-    validates :title, presence: true   
-    validates :body, presence: true, length: { minimum: 10 }
+    validates :title, presence: true, length: { in: 3..30 }
+    validates :body, presence: true, length: { in: 5..255 }
 end
