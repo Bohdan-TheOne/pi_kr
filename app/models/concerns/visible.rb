3a56834a2ea00583783ed5db57341651299a9071
Bohdan Ch <BohdanCh.the.One@gmail.com>
2021-05-24 18:25:58 +0300

    change visibility variants

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -1,13 +1,19 @@
 module Visible
   extend ActiveSupport::Concern
 
-  VALID_STATUSES = ['public', 'private', 'archived']
+  VALID_STATUSES = ['public', 'private']
 
   included do
     validates :status, inclusion: { in: VALID_STATUSES }
   end
 
+  def public?
+    status == 'public'
+  end
   def archived?
     status == 'archived'
+  end
+  def private?
+    status == 'private'
   end
 end
