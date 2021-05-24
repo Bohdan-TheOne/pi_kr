016aad8f189e28c59d4598b47687dbcd8d46adbf
Bohdan Ch <BohdanCh.the.One@gmail.com>
2021-05-25 01:13:10 +0300

    Add role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :validatable
   has_many :articles, foreign_key: :author_id, inverse_of: :author
+  ROLES = {
+    user: 0,
+    moder: 1,
+    admin: 2
+  }.with_indifferent_access.freeze
+
+  enum role: ROLES
 end
